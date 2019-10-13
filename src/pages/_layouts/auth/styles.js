79548529a1a357f90dc77e2eaff353f7e1bd4999
lukassxp <lpalucas.10@gmail.com>
2019-10-13 02:39:48 -0300

    added schema validation for sing in and sing up form data

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -33,6 +33,13 @@ export const Content = styled.div`
       }
     }
 
+    span {
+      color: #fb6f91;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
     button {
       height: 44px;
       border: 0;
